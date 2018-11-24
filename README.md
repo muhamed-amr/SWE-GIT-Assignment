d0ae6903e033da96d06535a686428bffd7790b02
Omar <omarh.ismail1@gmail.com>
2018-11-25 00:32:59 +0200

    Implemented Main Menu

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 1-Please use descriptive names for your commits/pull requests.
 
-2-If you have any questions, edit this README, and I will check it.
+2-If your function requires to modify the array, make a copy of the array and edit this copy not the original array.
+
+3-Don't forget to add your case number ( according to print menu ) and your function in the switch statement.
+
+4-Also add your function in the executeAll case (i.e case 18).
+
+5-Print your function name to the console before printing the output.
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,57 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    static int []arr;
+    static int size;
+
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.print("Enter The Size of the Array: ");
+        Scanner scanner = new Scanner(System.in);
+        size = scanner.nextInt();
+        System.out.println("Enter "+size+" Integers");
+        arr = new int[size];
+        for ( int i=0;i<size;i++){
+            arr[i] = scanner.nextInt();
+        }
+        printMenu();
+        int choice = scanner.nextInt();
+        switch (choice)
+        {
+            // Add Your Function case and call it
+            case 1:
+                break;
+            case 18: // Execute All
+                // Add Your Function Here
+                break;
+        }
+
+    }
+
+    private static void printMenu() {
+
+        System.out.println("Choose the number of operation you want to make:");
+        System.out.println("1-\tMost repeated value");
+        System.out.println("2-\tSort");
+        System.out.println("3-\tShuffle");
+        System.out.println("4-\tFind the largest prime");
+        System.out.println("5-\tFind the smallest prime");
+        System.out.println("6-\tCheck palindrome");
+        System.out.println("7-\tCheck sorted");
+        System.out.println("8-\tCount primes");
+        System.out.println("9-\tReverse array");
+        System.out.println("10-\tShift array");
+        System.out.println("11-\tDistinct array");
+        System.out.println("12-\tGet the maximum 3 numbers");
+        System.out.println("13-\t Get the minimum 3 numbers");
+        System.out.println("14-\tGet average");
+        System.out.println("15-\tGet median");
+        System.out.println("16-\tReturn only primes");
+        System.out.println("17-\tZero if less than zero");
+        System.out.println("18-\tExecute All");
+
     }
 }
