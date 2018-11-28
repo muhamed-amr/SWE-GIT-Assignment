eb7b86a32781a5c396be07a11236bd499832f91d
VRanaV <41893299+VRanaV@users.noreply.github.com>
2018-11-28 22:20:34 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -61,6 +61,7 @@ public class Main {
                 mostRepeatedValue();
                 zeroIfNegative();
                 countPrime();
+                GetSmallestPrime();
                 break;
         }
 
@@ -176,4 +177,40 @@ public class Main {
         System.out.println(c_freq);
         System.out.println("\n=======================================");
     }
+public static void GetSmallestPrime() {
+	if(isString)
+	{
+ 		System.out.println("the function doesn't work on strings");
+ 		System.out.println("\n=======================================");
+ 		return;
+ 	}
+	System.out.println(" Finding Smallest Prime ");
+	ArrayList<Integer>primes=new ArrayList<Integer>();
+	for(int i=0 ; i<arr.length;i++) {
+		boolean flag=false;
+		if(arr[i]==0||arr[i]==1) {
+			flag=true;
+			
+		}
+		
+		for(int j=2; j<arr[i]/2 ;j++) {
+			if(arr[i]%j==0) {
+				flag=true;
+				break;
+			}
+		}
+		
+		if(!flag) {primes.add(arr[i]);}
+	}
+		  int minValue = primes.get(0);
+		  int indexx=0;
+		  for(int i=1;i<primes.size();i++){
+		    if(primes.get(i)< minValue){
+			  minValue = primes.get(i);
+			  indexx=i;
+			}
+		  }
+		  System.out.println(primes.get(indexx));
+		}
+
 }
